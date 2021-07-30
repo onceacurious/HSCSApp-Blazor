05c098e6e7f8d6e8c447c2b46d95ab240dd40eee
Francis Deo Aurelio <francisdeoaurelio.fda@gmail.com>
2021-07-30 14:06:59 +0800

    Add BlazorIntersectionObserver

--- HSCSApp/Client/wwwroot/js/app.js
+++ HSCSApp/Client/wwwroot/js/app.js
@@ -1,1 +1,35 @@
 console.log("App.js is active");
+
+const btn = document.querySelector(".contact-btn");
+
+function msg() {
+  alert("Close btn clicked");
+}
+
+//Intersection Observer
+
+function myObserver() {
+  const sm = document.querySelector(".social-media");
+  const footer = document.querySelector(".footer");
+
+  const options = {
+    rootMargin: "-20px 0px 0px 0px",
+  };
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log(entry);
+        sm.style.backgroundColor = "black";
+      }
+
+      // console.log(entry);
+    });
+  }, options);
+
+  observer.observe(footer);
+  console.log("My observer");
+}
+
+function NavChange() {}
+
+// function myObserver(entries, observer) {}
